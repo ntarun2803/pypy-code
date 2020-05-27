507dccead3996f5d7216b30a4f38003710550500
ntarun2803 <ntarun2803@gmail.com>
2020-05-27 15:36:50 +0000

    a breif introduction to variables with python

---
+++ variables.py
@@ -0,0 +1,20 @@
+# hello nd welcome to variables
+# imagine a variable like a cup , it stores a value
+
+welcome="welcome to my program"
+# this is an example of a variable
+# you name the variable
+# then specify its value using the equals operator
+
+# in python there are different types of variables
+age=14 # this is an integer type
+name="youre name" # this is a string, a string is basically a sequence of individual charachters, space is also a charachter
+speed=14.5 # this is a float, a float is basically a decimal value
+is_alive = True # this is a boolean, a boolean is a value which basically stores only two things they are true and false
+
+# so those are variables in python
+# you can see the types of those variables by using the type function
+print(type(age))
+print(type(name))
+print(type(speed))
+print(type(is_alive))
